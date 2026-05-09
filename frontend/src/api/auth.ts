f9b3fc0a191b9efe45f74eb92b46b5a7bdf05bdd
M-Nusrat-Ullah <nusrat15-10498@diu.edu.bd>
2026-05-09 13:13:01 +0600

    feat: setup frontend with Vite TypeScript Tailwind and API layer

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,19 @@
+import api from "./axios";
+import { AuthResponse } from "../types";
+
+export const register = async (data: {
+  name: string;
+  email: string;
+  password: string;
+}): Promise<AuthResponse> => {
+  const response = await api.post("/auth/register", data);
+  return response.data;
+};
+
+export const login = async (data: {
+  email: string;
+  password: string;
+}): Promise<AuthResponse> => {
+  const response = await api.post("/auth/login", data);
+  return response.data;
+};
---
+++ frontend/src/types/index.ts
@@ -0,0 +1,39 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  role: "user" | "admin";
+}
+
+export interface Book {
+  _id: string;
+  title: string;
+  author: string;
+  description: string;
+  genre: string;
+  publishedYear: number;
+  rating: number;
+  coverImage?: string;
+  addedBy: {
+    _id: string;
+    name: string;
+    email: string;
+  };
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface AuthResponse {
+  success: boolean;
+  message: string;
+  data: {
+    user: User;
+    token: string;
+  };
+}
+
+export interface BooksResponse {
+  success: boolean;
+  count: number;
+  data: Book[];
+}
